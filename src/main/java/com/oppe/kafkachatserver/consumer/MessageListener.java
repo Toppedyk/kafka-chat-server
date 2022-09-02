e044e7de009e8d7a4dfcd7f0e2307f871f1f39d3
Taylor.Oppedyk <taylor.oppedyk@allata.com>
2022-09-02 09:38:36 -0600

    Server

---
+++ src/main/java/com/oppe/kafkachatserver/consumer/MessageListener.java
@@ -0,0 +1,24 @@
+package com.oppe.kafkachatserver.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import com.oppe.kafkachatserver.constants.KafkaConstants;
+import com.oppe.kafkachatserver.model.Message;
+
+@Component
+public class MessageListener {
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        System.out.println("sending via kafka listener..");
+        template.convertAndSend("/topic/group", message);
+    }
+}
